ee0117f6cd1a51f79378eab1bbdf904dbd96b813
Nick B <37129975+abulijahpisces@users.noreply.github.com>
2018-07-24 17:09:03 -0500

    Create t_l_tbeta.py
    I’m a noobie developer, give me a hand or give me a break ,

---
+++ t_l_tbeta.py
@@ -0,0 +1,47 @@
+ime = input ('we welcome you to the trustless_truth_foundation')
+ime = input ('would you like to learn more about our project?:')
+ime = input ('decentralize the parent\nchild relationship')
+ime = input ('draconian interference produces tomorrows terrorists')
+import turtle
+t=turtle
+tp = turtle.pos()
+turtle.setpos(11,15)
+t.home()
+t.dot()
+t.fd(06.03); t.dot(14, 'blue'); t.fd(80)
+t.color('pink')
+t.stamp()
+t.fd(111.5)
+t.left(80)
+t.fd(31.4)
+t.left(80)
+t.fd(111.5)
+t.goto(0,0)
+t.pen(fillcolor='blue', pencolor='blue', pensize=11)
+sorted(t.pen().items())
+[('fillcolor', 'blue'),('outline', 1), ('pencolor',
+'purple'),('pendown', True), ('pensize',14),
+('resizemode','noresize'),('shearfactor', 0.0), ('shown', True),
+('speed', 9), ('stretchfactor', (1.0, 1.0)),('tilt', 0.0)]
+penstate=turtle.pen()
+t.color('blue','')
+t.penup()
+sorted(t.pen().items())[:3]
+[('fillcolor',''),('outline',1),('pencolor','blue')]
+t.pen(penstate, fillcolor='blue')
+sorted(t.pen().items())[:3]
+[('fillcolor','blue'), ('outline', 1), ('pencolor', 'purple')]
+t.circle(111.5)
+t.pencolor('blue')
+tup = (0.8,0.8,0.80)
+turtle.pencolor('pink')
+t.fd(11.15)
+t.right(06.03)
+t.fd(3.14)
+t.circle(03.01)
+turtle.pencolor('purple')
+t.fd(201.9)
+ime = input ('we welcome you to the trustless_truth_foundation')
+ime = input ('would you like to learn more about our project?:')
+ime = input ('decentralize the parent\nchild relationship')
+ime = input ('draconian interference produces tomorrows terrorists')
